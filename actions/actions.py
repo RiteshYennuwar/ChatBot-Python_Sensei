ff833c8f343814138068e97228a63c7ed049eec2
Sitaram <sitaramyeleswarapu@gmail.com>
2023-08-21 18:35:13 +0530

    Actions is working

--- actions/actions.py
+++ actions/actions.py
@@ -1,73 +1,60 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-#
-#
-class ActionHelloWorld(Action):
 
-    def name(self) -> Text:
-         return "action_explain_code"
-
-    def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        response=domain["responses"]["utter_ask_code_explanation"]
-        dispatcher.utter_message(response[0]["text"])
-
-        return []
-   
-    def name(self) -> Text:
-          return "action_provide_updating_steps"
-     
-    def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        response=domain["responses"]["utter_ask_how_to_update"]
-        dispatcher.utter_message(response[0]["text"])
-        dispatcher.utter_attachment("https://www.pythoncentral.io/how-to-update-python/")
-     
-        
-        return []
-
-
-
+class ActionProvideCodeExamples(Action):
     def name(self) -> Text:
         return "action_provide_code_examples"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        code_type = tracker.get_slot("code_type")  
-        
-        
-        code_examples = {
-            "dictionaries": "my_dict = {'key': 'value'}",
-            "for loop": "for item in my_list:\n    print(item)",
-            "while loop": "while condition:\n    # do something",
-            "if else": "if condition:\n    # do something\nelse:\n    # do something else",
-            "tuple": "my_tuple = (1, 2, 3)",
-            "list": "my_list = [1, 2, 3]",
-        }
-
-        if code_type in code_examples:
-            example_code = code_examples[code_type]
-            response = f"Sure! Here's an example of {code_type} in Python:\n\n{example_code}"
-        else:
-            response = "I'm sorry, I don't have an example for that code type."
-
+        # Replace this with your code logic to provide code examples
+        response = "Here are some code examples for the requested topic."
         dispatcher.utter_message(text=response)
         return []
 
-   
-   
+class ActionExplainCode(Action):
+    def name(self) -> Text:
+        return "action_explain_code"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Replace this with your code logic to explain code
+        dispatcher.utter_message(text="Sure! Here's an explanation for the code snippet.")
+        return []
+
+class ActionProvideErrorAssistance(Action):
+    def name(self) -> Text:
+        return "action_provide_error_assistance"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Replace this with your code logic to provide error assistance
+        dispatcher.utter_message(text="Here's some assistance for the error message or code snippet.")
+        return []
+
+class ActionExplainConcept(Action):
+    def name(self) -> Text:
+        return "action_explain_concept"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Replace this with your code logic to explain a concept
+        dispatcher.utter_message(text="Sure! Here's an explanation for the requested concept.")
+        return []
+
+class ActionProvideUpdatingSteps(Action):
+    def name(self) -> Text:
+        return "action_provide_updating_steps"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # Replace this with your code logic to provide updating steps
+        dispatcher.utter_message(text="Here are the steps to update Python or Pip.")
+        return []
+
+class ActionUtterGoodbye(Action):
+    def name(self) -> Text:
+        return "utter_goodbye"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_goodbye")
+        return []
+
+# Add more actions as needed for other parts of your domain
 
