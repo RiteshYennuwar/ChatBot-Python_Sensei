74e5c530b14a9d1deeaaff96ee010e4488b6bf45
Sitaram <sitaramyeleswarapu@gmail.com>
2023-08-21 15:02:27 +0530

    updated action.py

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,38 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
 #
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+         return "action_explain_code"
+
+    def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        response=domain["responses"]["utter_ask_code_explanation"]
+        dispatcher.utter_message(response[0]["text"])
+
+        return []
+   
+    def name(self) -> Text:
+          return "action_provide_updating_steps"
+     
+    def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        response=domain["responses"]["utter_ask_how_to_update"]
+        dispatcher.utter_message(response[0]["text"])
+        dispatcher.utter_attachment("https://www.pythoncentral.io/how-to-update-python/")
+     
+        
+        return []
+   
+
