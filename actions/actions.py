1beadab134a91f46a92c1439e5f031ad7c6a8fdb
RiteshYennuwar <yennuwarritesh@hotmail.com>
2023-08-22 11:16:39 +0530

    bot can seach through web page and give responce

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,8 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import requests
+from bs4 import BeautifulSoup
 
 class ActionProvideCodeExamples(Action):
     def name(self) -> Text:
@@ -8,8 +10,32 @@ class ActionProvideCodeExamples(Action):
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         # Replace this with your code logic to provide code examples
-        response = "Here are some code examples for the requested topic."
-        dispatcher.utter_message(text=response)
+        try:
+            # Get the user's query from the tracker
+            user_query = tracker.latest_message.get('text')
+
+            # Search the Python documentation
+            search_url = f"https://docs.python.org/3/search.html?q={user_query}"
+            response = requests.get(search_url)
+            response.raise_for_status()
+
+            # Parse the search results page using BeautifulSoup
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Extract the first search result's title and URL
+            first_result = soup.find('dt', class_='search-results')
+            if first_result:
+                result_title = first_result.find('a').text
+                result_url = first_result.find('a')['href']
+                full_url = f"https://docs.python.org/3/{result_url}"
+
+                # Send the response back to the user
+                dispatcher.utter_message(text=f"I found a relevant Python documentation page: {result_title}\nYou can read more about it here: {full_url}")
+            else:
+                dispatcher.utter_message(text="I couldn't find any relevant information in the Python documentation.")
+        except Exception as e:
+            dispatcher.utter_message(text="An error occurred while searching the Python documentation.")
+
         return []
 
 class ActionExplainCode(Action):
